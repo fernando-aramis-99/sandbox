a2fcb92ae2dcdf88d6bc03f5def2c2df26fcb1ff
fernando-aramis-99 <fernandoaramis.99@gmail.com>
2023-01-22 20:46:59 -0300

    adicionando o componente ngOnInit

--- Angular/meu-primeiro-projeto/src/app/app-routing.module.ts
+++ Angular/meu-primeiro-projeto/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 
 const routes: Routes = [];
 
@@ -7,4 +8,15 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+
 export class AppRoutingModule { }
+export class HomeComponent implements OnInit {
+  constructor() { }
+  ngOnInit() {}
+}
