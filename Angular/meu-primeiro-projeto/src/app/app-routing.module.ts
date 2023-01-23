91e343958acdc704c8d468d4b21f7c1da3ce4dff
fernando-aramis-99 <fernandoaramis.99@gmail.com>
2023-01-22 21:13:32 -0300

    criando um titulo de bem-vindo

--- Angular/meu-primeiro-projeto/src/app/app-routing.module.ts
+++ Angular/meu-primeiro-projeto/src/app/app-routing.module.ts
@@ -9,14 +9,14 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-})
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.scss'],
+// })
 
 export class AppRoutingModule { }
-export class HomeComponent implements OnInit {
-  constructor() { }
-  ngOnInit() {}
-}
+// export class HomeComponent implements OnInit {
+//   constructor() { }
+//   ngOnInit() {}
+// }
