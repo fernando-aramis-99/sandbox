ac844314d25d2e50f614d9268fcdf6b50c66089d
fernando-aramis-99 <fernandoaramis.99@gmail.com>
2023-01-27 02:29:16 -0300

    atualização de codigos de aritmetica

---
+++ Python/training/math_operations.py
@@ -0,0 +1,18 @@
+def soma(number_a: int, number_b: int):
+    return number_a + number_b
+
+
+def resta(number_a: int, number_b: int):
+    return number_a - number_b
+
+
+def mult(number_a: int, number_b: int):
+    return number_a * number_b
+
+
+def div(number_a: int, number_b: int):
+    return number_a / number_b
+
+
+def mod(number_a: int, number_b: int):
+    return number_a % number_b
