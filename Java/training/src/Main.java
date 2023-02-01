ed0d159215036d76bc4c2d4121448e94ee47fa71
fernando-aramis-99 <fernandoaramis.99@gmail.com>
2023-02-01 12:02:48 -0300

    atualizando arquivos em java

--- Java/training/src/Main.java
+++ Java/training/src/Main.java
@@ -1,12 +1,14 @@
 public class Main
 {
     private static Lista_01 lista = new Lista_01();
+    private static Unirio_lista unilista = new Unirio_lista();
     private static int valor = -1;
 
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
         menu_lista();
+        menu_unilista();
     }
     public static void menu_lista()
     {
@@ -24,6 +26,30 @@ public class Main
             {
                 case 0: break;
                 case 1: lista.idade_dias();break;
+                case 2: lista.media_aritmetica();break;
+                case 3: lista.ajuste_saldo();break;
+                case 4: lista.quant_salario();break;
+                case 5: lista.ante_sucessor();break;
+                default: System.out.println("! Seleção Invalida !");break;
+            }
+        } while (valor != 0);
+    }
+    public static void menu_unilista()
+    {
+        do
+        {
+            System.out.print("Menu - Unirio Lista" +
+                    "\n1 - Contador de pares" +
+                    "\n2 - *Calcular média aritmética" +
+                    "\n3 - Reajuste de saldo" +
+                    "\n4 - Quantidade de salários" +
+                    "\n5 - Antecessor e Sucessor" +
+                    "\n0 - Sair" +
+                    "\n  - Selecione o programa: ");
+            switch (valor)
+            {
+                case 0: break;
+                case 1: unilista.contador_pares();break;
                 case 2: lista.media_aritmetica();break;
                 case 3: lista.ajuste_saldo();break;
                 case 4: lista.quant_salario();break;
---
+++ Java/training/src/Unirio_lista.java
@@ -0,0 +1,11 @@
+public class Unirio_lista
+{
+    public void contador_pares()
+    {
+//        totalPares: a função recebe como parâmetro um array de inteiros e retorna um  número inteiro indicando o total de números pares existentes no array.
+
+        String array;
+        System.out.print("Digite um numero inteiro: ");
+
+    }
+}
